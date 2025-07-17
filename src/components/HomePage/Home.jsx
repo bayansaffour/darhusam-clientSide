7140aa130143e2eab793b8c10b1ee378b36927d2
bayansaffour <bayansaffour9298@gmail.com>
2025-07-17 18:12:26 +0300

    lkj

--- src/components/HomePage/Home.jsx
+++ src/components/HomePage/Home.jsx
@@ -1,22 +1,16 @@
 import React from 'react'
 import Hero from './Hero'
 import WhoWeAre from './WhoWeAre'
-import Programs from './Programs'
-import News from './News'
 import Initiatives from './Initiatives'
 import SuccessStories from './SuccessStories'
 import Gallery from './Gallery'
-import NewsTicker from './NewsTicker'
 
 function Home() {
   return (
     <>
-    <NewsTicker/>
     <Hero/>
     <WhoWeAre/>
-    <Programs/>
     <Gallery/>
-    <News/>
     <SuccessStories/>
     <Initiatives/>
 
